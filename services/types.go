6a2b148ba3624f573b1e76fc1598cfa5ef3b769f
Толли <ziphyr.xyeag@gmail.com>
2021-09-24 22:04:45 +0300

    Update types.go

--- services/types.go
+++ services/types.go
@@ -25,7 +25,7 @@ import (
 const (
 	// NodeVersion is the version of
 	// whive core we are using.
-	NodeVersion = "1.0.2"
+	NodeVersion = "2.0.0"
 
 	// HistoricalBalanceLookup indicates
 	// that historical balance lookup is supported.
